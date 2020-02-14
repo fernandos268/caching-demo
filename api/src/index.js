20e18e751e34d92c5adcb3c3992d595679bce62c
GabrielM <gabriel@dnamicro.com>
2020-02-15 03:26:17 +0800

    Initial Express Config

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const { PORT } = process.env;
+
+const app = express();
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+})
