2b050163893565db13673ffeb4224ceade81cf85
GabrielM <gabriel@dnamicro.com>
2020-02-15 06:47:31 +0800

    Initial thinky-loader fork test

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,34 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan'
+import bodyParser from "body-parser";
+import logger from './core/logger/app-logger'
+import config from './core/config/config.dev'
+import cars from './routes/cars.route'
+import connectToDb from './db/connect';
 
-const { PORT } = process.env;
+const port = config.serverPort;
+logger.stream = {
+  write: function(message, encoding) {
+    logger.info(message);
+  }
+};
+
+connectToDb();
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev", { "stream": logger.stream }));
+app.use('/cars', cars)
 
-app.listen(PORT, () => {
-  console.log(`Express server listening on port ${PORT}`);
-})
+//Index route
+app.get('/', (req, res) => {
+  res.send('Invalid endpoint!');
+});
+
+app.listen(port, () => {
+  logger.info('server started - ', port);
+});
---
+++ api/src/schemas/index.js
@@ -0,0 +1,5 @@
+import Car from './car.model'
+
+export default {
+  Car
+}
