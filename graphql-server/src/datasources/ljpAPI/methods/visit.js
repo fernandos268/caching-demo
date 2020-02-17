8b8fceea84e1e2453bc6a132e0fd0aa3b054980b
JayR <goldenjayr@gmail.com>
2020-02-18 07:44:48 +0800

    new changes

--- graphql-server/src/datasources/ljpAPI/methods/visit.js
+++ graphql-server/src/datasources/ljpAPI/methods/visit.js
@@ -1,26 +1,22 @@
 async function getVisits(params) {
-    let result
     try {
-        result = await this.get(`/visit`, params)
+        return await this.get(`/visit`, params)
 
     } catch(err) {
         console.log('Error in getting visits:', err)
         throw err
     }
-
-    return result
 }
 
 async function getVisitsByProject({id, params}) {
-    let result
     try {
-        result = await this.get(`/project/${id}/visits`, params)
+        return await this.get(`/project/${id}/visits`, params)
     } catch(err) {
         console.log('Error in getting visits by projects', err)
         throw err
     }
-    return result
 }
+
 
 module.exports = {
     getVisits,
