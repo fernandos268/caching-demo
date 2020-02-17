7300b9145e89d399ec26c1037f1ca55642f35efb
Fernando Singson Jr <fernando@dnamicro.com>
2020-02-18 07:45:39 +0800

    Projects CRUD

--- graphql-server/src/datasources/ljpAPI/methods/photo.js
+++ graphql-server/src/datasources/ljpAPI/methods/photo.js
@@ -1,34 +1,55 @@
 async function getPhotos(params) {
-    let result
-    try{
-        const {
-            limit,
-            limitless,
-            page
-        } = params
 
-        const query_string = `?limit=${limit}&page=${page}`
-
-        result = await this.get(`/photo${query_string}`)
-    }catch(err) {
-        console.log('Error in fetching photos: ', err)
+    try {
+        return await this.get(`/photo`, params)
+    } catch (err) {
+        console.log('Error in fetching Photos: ', err)
         throw err
     }
-
-    return result
 }
 
-async function getPhotosByVisit(visit_id) {
-    let result
+async function getPhotoById(id) {
     try {
-        result = await rethink.table('tbl_Photo').filter({ visit_id }).run()
+        return await this.get(`/photo/${id}`)
     } catch (err) {
-        console.log('Error in getting photos by Visit', err)
+        console.log('Error in fetching Photos: ', err)
+        throw err
     }
-    return result
 }
+
+async function createPhoto(input) {
+    try {
+        return await this.post(`photo`, input)
+    } catch (err) {
+        console.log('Error in creating photo', err)
+        throw err
+    }
+}
+
+async function updatePhoto(input) {
+    const { id } = input
+    try {
+        return await this.put(`photo/${id}`, input)
+    } catch (err) {
+        console.log('Error in updating photo', err)
+        throw err
+    }
+}
+
+async function deletePhoto(id) {
+    try {
+        return await this.delete(`photo/${id}`)
+    } catch (err) {
+        console.log('Error in deleting photo', err)
+        throw err
+    }
+}
+
 
 module.exports = {
     getPhotos,
-    getPhotosByVisit
+    getPhotoById,
+    createPhoto,
+    updatePhoto,
+    deletePhoto
 }
--- graphql-server/src/resolvers/project.js
+++ graphql-server/src/resolvers/project.js
@@ -7,19 +7,19 @@ module.exports = {
     Mutation: {
         async createProject(_, { input }, { dataSources }) {
             console.log('Creating project.....')
-           return await dataSources.ljpAPI.createProject(input)
+            return await dataSources.ljpAPI.createProject(input)
         },
         async updateProject(_, { input }, { dataSources }) {
-
+            return await dataSources.ljpAPI.updateProject(input)
         },
-        async deleteProject(_, { input }, { dataSources }) {
-
+        async deleteProject(_, { id }, { dataSources }) {
+            return await dataSources.ljpAPI.deleteProject(id)
         }
     },
     Project: {
-        async visits({id}, __, { dataSources }, {variableValues: {params}}) {
+        async visits({ id }, __, { dataSources }, { variableValues: { params } }) {
             console.log(`Fetching visits by project id ${id}`)
-            return await dataSources.ljpAPI.getVisitsByProject({id, params});
+            return await dataSources.ljpAPI.getVisitsByProject({ id, params });
         }
     }
 }
